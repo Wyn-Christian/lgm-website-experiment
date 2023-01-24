cf92c0263e48e0ac372dbb747fbe8dd14b470357
bluerenz123 <lsedutan30@gmail.com>
2023-01-25 02:00:48 +0800

    Home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,130 +9,126 @@ import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 
 import MenuIcon from "@mui/icons-material/Menu";
-import LogoImg from "../assets/Logo.jpg";
+import LogoImg from "../assets/navbar-logo.png";
 
 const LinkPage = ({ title, to }) => {
-  return (
-    <Button sx={{ color: "inherit" }} disableRipple>
-      <Link
-        to={to}
-        style={{
-          textDecoration: "none",
-          color: "inherit",
-        }}
-      >
-        {title}
-      </Link>
-    </Button>
-  );
+	return (
+		<Button sx={{ color: "inherit" }} disableRipple>
+			<Link
+				to={to}
+				style={{
+					textDecoration: "none",
+					color: "inherit",
+					fontSize: "22px",
+					textTransform: "capitalize",
+					fontWeight: "bold",
+				}}
+			>
+				{title}
+			</Link>
+		</Button>
+	);
 };
 
 const pages = [
-  { title: "Home", to: "/" },
-  { title: "About Us", to: "/about-us" },
-  { title: "Packages", to: "/packages" },
-  { title: "Gallery", to: "/gallery" },
-  { title: "Reviews", to: "/reviews" },
-  { title: "Contact Us", to: "/contact-us" },
-  { title: "FaQs", to: "/faqs" },
+	{ title: "Home", to: "/" },
+	{ title: "About Us", to: "/about-us" },
+	{ title: "Packages", to: "/packages" },
+	{ title: "Gallery", to: "/gallery" },
+	{ title: "Reviews", to: "/reviews" },
+	{ title: "Contact Us", to: "/contact-us" },
+	{ title: "FAQs", to: "/faqs" },
 ];
 
 function NavBar() {
-  const theme = useTheme();
-  const [anchorElNav, setAnchorElNav] = useState(null);
+	const theme = useTheme();
+	const [anchorElNav, setAnchorElNav] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  return (
-    <AppBar position="sticky">
-      <Container>
-        <Toolbar
-          disableGutters
-          sx={{
-            height: "100px",
-          }}
-        >
-          <Box sx={{ display: { xs: "block", md: "none" } }}>
-            <IconButton
-              size="large"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-            sx={{
-              display: { xs: "flex", md: "none", color: "white" },
-            }}
-          >
-            {pages.map((p) => (
-              <MenuItem key={p.title} onClick={handleCloseNavMenu}>
-                <Typography textAlign="center" variant="h6">
-                  <LinkPage {...p} />
-                </Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: "flex",
-              justifyContent: "center",
-              height: "90px",
-            }}
-          >
-            <img
-              src={LogoImg}
-              alt="Logo"
-              style={{ borderRadius: "50%" }}
-            />
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: {
-                xs: "none",
-                md: "flex",
-              },
-              gap: "18px",
-            }}
-          >
-            {pages.map((p) => (
-              <LinkPage {...p} key={p.title} />
-            ))}
-            {/* <LinkPage title="Home" to="/" />
+	return (
+		<AppBar position="static" sx={{ marginBottom: "10px" }}>
+			<Container>
+				<Toolbar
+					disableGutters
+					sx={{
+						height: "220px",
+						display: "flex",
+						flexDirection: "column",
+					}}
+				>
+					<Box sx={{ display: { xs: "block", md: "none" } }}>
+						<IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
+							<MenuIcon fontSize="large" />
+						</IconButton>
+					</Box>
+					<Menu
+						id="menu-appbar"
+						anchorEl={anchorElNav}
+						anchorOrigin={{
+							vertical: "bottom",
+							horizontal: "left",
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: "top",
+							horizontal: "left",
+						}}
+						open={Boolean(anchorElNav)}
+						onClose={handleCloseNavMenu}
+						sx={{
+							display: { xs: "flex", md: "none", color: "white" },
+						}}
+					>
+						{pages.map((p) => (
+							<MenuItem key={p.title} onClick={handleCloseNavMenu}>
+								<Typography textAlign="center" variant="h6">
+									<LinkPage {...p} />
+								</Typography>
+							</MenuItem>
+						))}
+					</Menu>
+
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+						}}
+					>
+						<img src={LogoImg} alt="Logo" style={{ borderRadius: "50%" }} width={350} />
+					</Box>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: {
+								xs: "none",
+								md: "flex",
+							},
+							gap: "18px",
+						}}
+					>
+						{pages.map((p) => (
+							<LinkPage {...p} key={p.title} />
+						))}
+						{/* <LinkPage title="Home" to="/" />
             <LinkPage title="About Us" to="/about-us" />
             <LinkPage title="Packages" to="/packages" />
             <LinkPage title="Gallery" to="/gallery" />
             <LinkPage title="Reviews" to="/reviews" />
             <LinkPage title="Contact Us" to="/contact-us" />
             <LinkPage title="FaQs" to="/faqs" /> */}
-          </Box>
-          <Box sx={{ flexGrow: { xs: 0, md: 1 } }} />
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+					</Box>
+					<Box sx={{ flexGrow: { xs: 0, md: 1 } }} />
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 
 export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,68 @@
+import { Grid, useTheme } from "@mui/material";
 import { Box, Container, Typography } from "@mui/material";
 import { useScroll, useSpring, animated } from "@react-spring/web";
+import HomeImg from "../assets/home-img.jpg";
+import ImgLast from "../assets/image-below.jpg";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 
 function Home() {
-  const props = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-  });
-  const AnimatedTypography = animated(Typography);
-  return (
-    <>
-      <Container sx={{ minHeight: "100vh" }}>
-        <Typography variant="h3" textAlign="center">
-          We will make your "once in a lifetime" be memorable for lifetime.
-        </Typography>
-        <AnimatedTypography variant="h2" style={props}>
-          Example sheesh
-        </AnimatedTypography>
-      </Container>
-    </>
-  );
+	const theme = useTheme();
+	const props = useSpring({
+		opacity: 1,
+		from: { opacity: 0 },
+	});
+	const AnimatedTypography = animated(Typography);
+	return (
+		<>
+			<Box sx={{ minHeight: "100vh", textAlign: "center" }}>
+				<Box>
+					<img src={HomeImg} alt="Logo" style={{ width: "70%", margin: "0" }} />
+				</Box>
+				<Box sx={{ color: "#fff", width: "100%", display: "flex", flexDirection: "column", alignItems: "center", backgroundColor: theme.palette.primary.contrastText, padding: "80px 0" }}>
+					<Typography variant="h2" sx={{ textAlign: "center" }}>
+						Let's Get Mary
+					</Typography>
+					<Typography sx={{ fontSize: "20px", textAlign: "justify", width: "50%", lineHeight: "30px", paddingBottom: "20px" }} mt="40px">
+						Let's Get Mary Events Management is opening the year of 2023 with a blast of Fresh and New Wedding Packages that will surely save some coins and treasure memories of your "once in a
+						lifetime" bridal extravaganzas!
+					</Typography>
+					<Typography sx={{ fontSize: "20px", textAlign: "justify", width: "50%", lineHeight: "30px" }}>
+						Feel free to check and avail our wonderful wedding packages. Your in great hands our dear Future LGM couples because here in LGM "We make your once in a lifetime be memorable for a
+						lifetime".{" "}
+					</Typography>
+				</Box>
+				<Box sx={{ margin: "80px 0" }}>
+					<Typography variant="h2" sx={{ textAlign: "center", marginBottom: "50px" }}>
+						We are known for being...
+					</Typography>
+					<Box sx={{ margin: "0 20%" }}>
+						<Grid container spacing={8} justifyContent="center">
+							<Grid item xs={12} md={4}>
+								<EmojiObjectsIcon sx={{ fontSize: "80px", color: "#135133" }} />
+								<Typography sx={{ fontSize: "30px" }}>Creative</Typography>
+								<Typography sx={{ fontSize: "20px", textAlign: "justify" }}>We serve the best of the best unique ideas for each and every event we make.</Typography>
+							</Grid>
+							<Grid item xs={12} md={4}>
+								<CheckCircleIcon sx={{ fontSize: "80px", color: "#135133" }} />
+								<Typography sx={{ fontSize: "30px" }}>Reliable</Typography>
+								<Typography sx={{ fontSize: "20px", textAlign: "justify" }}>We are DTI registered business, therefore, reliability is never a concern on us.</Typography>
+							</Grid>
+							<Grid item xs={12} md={4}>
+								<AutoAwesomeIcon sx={{ fontSize: "80px", color: "#135133" }} />
+								<Typography sx={{ fontSize: "30px" }}>Engaging</Typography>
+								<Typography sx={{ fontSize: "20px", textAlign: "justify" }}>We make sure that your guests will be engaged and entertained during your event.</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Box>
+					<img src={ImgLast} alt="Logo" style={{ width: "100%", margin: "0", objectFit: "contain" }} />
+				</Box>
+			</Box>
+		</>
+	);
 }
 
 export default Home;
